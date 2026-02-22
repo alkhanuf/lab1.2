385a115f5ed1ee60ea9a28add7a8794dc50bd13f
alkhanuf <simoon.phv@gmail.com>
2026-02-22 20:06:41 +0800

    расчет оценок

--- lab12/Program.cs
+++ lab12/Program.cs
@@ -19,8 +19,35 @@ namespace lab12
 
             for (int i = 0; i < gradesArr.Length; i++)
             {
-                Console.WriteLine(gradesArr[i]);
+                int grade = int.Parse(gradesArr[i]);
+
+                if (grade == 5)
+                {
+                    count5++;
+                }
+                else if (grade == 4)
+                {
+                    count4++; 
+                }
+                else if (grade == 3)
+                {
+                    count3++;
+                }
+                else if (grade == 2)
+                {
+                    count2++;
+                }
+                else
+                {
+                    Console.WriteLine("оценки " + grade + " не существует");
+                }
             }
+
+            Console.WriteLine("пятерок: " + count5);
+            Console.WriteLine("четверток: " + count4);
+            Console.WriteLine("троек: " + count3);
+            Console.WriteLine("двоек: " + count2);
+
         }
     }
 }
