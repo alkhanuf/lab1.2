c253bd623776446d29ebdf82693d3547996e4cf2
alkhanuf <simoon.phv@gmail.com>
2026-02-24 21:35:09 +0800

    декомпозиция

---
+++ lab12/Logic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Logic
+{
+    public static int[] CountGrades(string[] grades)
+    {
+        int count5 = 0;
+        int count4 = 0;
+        int count3 = 0;
+        int count2 = 0;
+
+
+        for (int i = 0; i < grades.Length; i++)
+        {              
+            int grade = int.Parse(grades[i]);
+
+            if (grade == 5)
+            {
+                count5++;
+            }
+            else if (grade == 4)
+            {
+                count4++;
+            }
+            else if (grade == 3)
+            {
+                count3++;
+            }
+            else if (grade == 2)
+            {
+                count2++;
+            }          
+        }
+
+        return new int[] { count5, count4, count3, count2 };
+    }
+}
--- lab12/Program.cs
+++ lab12/Program.cs
@@ -12,42 +12,12 @@ namespace lab12
 
             string[] gradesArr = grades.Split(' ');
 
-            int count5 = 0;
-            int count4 = 0;
-            int count3 = 0;
-            int count2 = 0;
+            int[] counts = Logic.CountGrades(gradesArr);
 
-            for (int i = 0; i < gradesArr.Length; i++)
-            {
-                int grade = int.Parse(gradesArr[i]);
-
-                if (grade == 5)
-                {
-                    count5++;
-                }
-                else if (grade == 4)
-                {
-                    count4++; 
-                }
-                else if (grade == 3)
-                {
-                    count3++;
-                }
-                else if (grade == 2)
-                {
-                    count2++;
-                }
-                else
-                {
-                    Console.WriteLine("оценки " + grade + " не существует");
-                }
-            }
-
-            Console.WriteLine("пятерок: " + count5);
-            Console.WriteLine("четверток: " + count4);
-            Console.WriteLine("троек: " + count3);
-            Console.WriteLine("двоек: " + count2);
-
+            Console.WriteLine("пятерок: " + counts[0]);
+            Console.WriteLine("четверок: " + counts[1]);
+            Console.WriteLine("троек: " + counts[2]);
+            Console.WriteLine("двоек: " + counts[3]);
         }
     }
 }
